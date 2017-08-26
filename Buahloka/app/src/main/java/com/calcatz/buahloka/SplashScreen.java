9e6f71b98212dbcbdf858d049611ffb25627abc7
Muhammad Fauzan <fauzan_htf@ymail.com>
2017-08-26 10:28:06 +0700

    Add some activity and fragment
    activity : splashscreen, main, login, register
    fragment : home, profile, edit profile

---
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.calcatz.buahloka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_activity);
+    }
+}
