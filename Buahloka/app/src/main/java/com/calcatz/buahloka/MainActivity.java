14ec655daa81e17e969f4c5a498ee865ab208bc1
Muhammad Fauzan <fauzan_htf@ymail.com>
2017-08-31 19:11:19 +0700

    samid main activity nav draw

--- Buahloka/app/src/main/java/com/calcatz/buahloka/MainActivity.java
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/MainActivity.java
@@ -52,16 +52,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tv_bualoka = (TextView)findViewById(R.id.tx_welcome);
-        //iseng aja nambahin welcome to bualoka
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                tv_bualoka.setVisibility(View.GONE);
-
-            }
-        },2*1000);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -75,15 +65,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //tab fragment
-//        toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//        viewPager = (ViewPager) findViewById(R.id.viewpager);
-//        setupViewPager(viewPager);
-//
+
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -152,14 +135,27 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_logout) {
             LoginActivity loginActivity = new LoginActivity();
             loginActivity.SignOut();
-        } else if (id == R.id.nav_home) {
-
         } else if (id == R.id.nav_history) {
-
+            HistoryFragment historyFragment = new HistoryFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction()
+                    .replace(R.id.fragment_view, historyFragment)
+                    .commit();
         } else if (id == R.id.nav_toko) {
-
+            Intent toko = new Intent(MainActivity.this, TokoActivity.class);
+            startActivity(toko);
         } else if (id == R.id.nav_about) {
-
+            AboutFragment aboutFragment = new AboutFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction()
+                    .replace(R.id.fragment_view, aboutFragment)
+                    .commit();
+        } else if (id == R.id.nav_profile){
+            ProfileFragment profileFragment = new ProfileFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction()
+                    .replace(R.id.fragment_view, profileFragment)
+                    .commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
