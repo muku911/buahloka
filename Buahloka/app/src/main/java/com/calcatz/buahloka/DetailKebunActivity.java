85d705210c60a300ed94fa78035677763859c9b7
Husni Mubarok <husnimubarok911@gmail.com>
2017-09-02 16:52:44 +0700

    ReviewLayout

--- Buahloka/app/src/main/java/com/calcatz/buahloka/DetailKebunActivity.java
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/DetailKebunActivity.java
@@ -7,13 +7,16 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailKebunActivity extends AppCompatActivity {
 
     //UI
     private TextView tx_judulKebun, tx_harga, tx_tersedia, tx_promo, tx_isiDiPesan;
     private RatingBar rating;
     private ImageView img_logoBuah;
-    private Button btn_buyItem;
+    private Button btn_buyItem, btn_review;
 
     private ImageView img_doc1, img_doc2, img_doc3;
     private TextView
@@ -29,6 +32,10 @@ public class DetailKebunActivity extends AppCompatActivity {
     //String
     private String pilihanKebun, pilihanBuah;
 
+    //Data
+    private Rating ratingThis;
+    private List<Rating>ratings = new ArrayList<Rating>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,52 @@ public class DetailKebunActivity extends AppCompatActivity {
 
         headerView();
         init();
+        //Reset
+        ratings.clear();
+        //set
+        setValueOffline();
+        //valuereset();
+        ratingAVG();
+
+
+
+    }
+
+    private void ratingAVG() {
+        float b = 0;
+        for (int a = 0 ; a < ratings.size() ; a++ ){
+            b = b + Float.parseFloat(ratings.get(a).getRating());
+        }
+        float c = b/5;
+        rating.setRating(c);
+
+        int sum = ratings.size();
+        tx_isiDiPesan.setText(""+sum+"x");
+    }
+
+    private void setValueOffline() {
+        ratingThis = new Rating("4");
+        ratings.add(ratingThis);
+
+        ratingThis = new Rating("1");
+        ratings.add(ratingThis);
+
+        ratingThis = new Rating("3");
+        ratings.add(ratingThis);
+
+        ratingThis = new Rating("5");
+        ratings.add(ratingThis);
+    }
+
+    private void valuereset() {
+        rating.setRating(0);
+        tx_harga.setText("Rp. "+" /Kg");
+        tx_tersedia.setText(" ");
+        tx_promo.setText("Rp. "+" /Kg Pemesanan > " + " ");
+        tx_isiDiPesan.setText(" ");
+        tx_judulKebun.setText("Bualoka");
+        img_logoBuah.setImageResource(R.drawable.logo_bualoka);
+
 
     }
 
@@ -51,6 +104,7 @@ public class DetailKebunActivity extends AppCompatActivity {
         img_logoBuah = (ImageView)findViewById(R.id.img_logoBuah);
         rating =  (RatingBar)findViewById(R.id.ratingBar);
         btn_buyItem = (Button)findViewById(R.id.btn_buyItem);
+        btn_review = (Button)findViewById(R.id.btn_review);
 
         tx_judulKebun = (TextView)findViewById(R.id.tx_judulKebun);
         tx_harga = (TextView)findViewById(R.id.tx_harga);
@@ -85,5 +139,21 @@ public class DetailKebunActivity extends AppCompatActivity {
         tx_isiKadaluarsa = (TextView)findViewById(R.id.tx_isiKadaluarsa);
 
         tx_isiManfaat = (TextView)findViewById(R.id.tx_isiManfaat);
+    }
+}
+
+class Rating {
+    private String rating;
+
+    public Rating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
     }
 }
---
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/ReviewActivity.java
@@ -0,0 +1,48 @@
+package com.calcatz.buahloka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
+
+class Pereview {
+    String name, rating, comment;
+
+    public Pereview(String name, String rating, String comment) {
+        this.name = name;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
+
