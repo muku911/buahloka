6deb4271bc173d35c677c0af284950aebfacfddc
Muhammad Fauzan <fauzan_htf@ymail.com>
2017-09-05 21:07:43 +0700

    Keranjang Done
    list keranjang, delete item, dan harga total keranjang

--- Buahloka/app/src/main/java/com/calcatz/buahloka/KeranjangFragment.java
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/KeranjangFragment.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,13 +39,16 @@ public class KeranjangFragment extends Fragment{
     FirebaseUser user = firebaseAuth.getCurrentUser();
 
     ListView lv_item_keranjang;
+    TextView tv_harga_total;
 
-    ArrayList<String> nama_barang = new ArrayList<>();
-    ArrayList<String> nama_toko = new ArrayList<>();
-    ArrayList<Integer> quantity = new ArrayList<>();
-    ArrayList<Integer> harga_barang = new ArrayList<>();
+    List<String> nama_barang = new ArrayList<>();
+    List<String> nama_toko = new ArrayList<>();
+    List<Integer> quantity = new ArrayList<>();
+    List<Integer> harga_barang = new ArrayList<>();
 
-    ArrayList<String> id_barang = new ArrayList<>();
+    List<IdBarang> id_barang = new ArrayList<IdBarang>();
+    List<Barang> l_barang = new ArrayList<Barang>();
+    List<String> id_item = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,15 +58,14 @@ public class KeranjangFragment extends Fragment{
 
         lv_item_keranjang = view.findViewById(R.id.lv_keranjang);
 
-        setValue();
+        tv_harga_total = view.findViewById(R.id.tv_harga_total);
 
-        KeranjangAdapter keranjangAdapter = new KeranjangAdapter(getActivity(),nama_barang,nama_toko,quantity,harga_barang);
-        lv_item_keranjang.setAdapter(keranjangAdapter);
+        setItem();
 
         return view;
     }
 
-    private void setValue(){
+    public void setItem() {
 //        nama_barang.add(0,"Jeruk");
 //        nama_barang.add(1,"Jambu");
 //
@@ -73,13 +77,43 @@ public class KeranjangFragment extends Fragment{
 //
 //        harga_barang.add(0,100000);
 //        harga_barang.add(1,200000);
-        DatabaseReference dr_nama_barang = database.getReference();
-        dr_nama_barang.child("User").child(user.getUid()).child("Keranjang").child("Item").addValueEventListener(new ValueEventListener() {
+        DatabaseReference dr_item = database.getReference();
+        dr_item.child("User").child(user.getUid()).child("Keranjang").child("Item").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot data : dataSnapshot.getChildren()){
-                    id_barang.add();
+                int hartot = 0;
+                if(id_barang!=null){
+                    id_barang.clear();
                 }
+                if(quantity!=null){
+                    quantity.clear();
+                }
+                if(harga_barang!=null){
+                    harga_barang.clear();
+                }
+                if(id_item!=null){
+                    id_item.clear();
+                }
+
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    IdBarang id = data.getValue(IdBarang.class);
+
+                    id_barang.add(id);
+                }
+
+                for (int i = 0; i < id_barang.size(); i++) {
+                    int qty = id_barang.get(i).getQuantity();
+                    int hrg = id_barang.get(i).getHarga();
+                    String id = id_barang.get(i).getId();
+                    hartot = hartot + hrg;
+
+                    id_item.add(id);
+                    quantity.add(qty);
+                    harga_barang.add(hrg);
+
+                    tv_harga_total.setText("Rp. " + String.valueOf(hartot));
+                }
+                setBarang();
             }
 
             @Override
@@ -88,18 +122,170 @@ public class KeranjangFragment extends Fragment{
             }
         });
     }
+
+    private void setBarang() {
+        DatabaseReference dr_barang = database.getReference();
+        dr_barang.child("Barang").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if(l_barang!=null){
+                    l_barang.clear();
+                }
+                if(nama_barang!=null){
+                    nama_barang.clear();
+                }
+
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String id = data.getKey();
+                    for (int i = 0; i < id_barang.size(); i++) {
+                        if (id.equals(id_barang.get(i).getId_barang())) {
+                            Barang barang = data.getValue(Barang.class);
+
+                            l_barang.add(barang);
+                        }
+                    }
+                }
+                for (int i = 0; i < l_barang.size(); i++) {
+                    String nama = l_barang.get(i).getName();
+
+                    nama_barang.add(nama);
+                }
+                setToko();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setToko(){
+        DatabaseReference dr_toko = database.getReference();
+        dr_toko.child("Toko").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if(nama_toko!=null){
+                    nama_toko.clear();
+                }
+
+                for (DataSnapshot data : dataSnapshot.getChildren()){
+                    String id = data.getKey();
+                    for (int i = 0; i<l_barang.size(); i++) {
+                        if (id.equals(l_barang.get(i).getId_toko())) {
+                            String toko_name = data.child("Profile").child("name").getValue(String.class);
+
+                            nama_toko.add(toko_name);
+                        }
+                    }
+                }
+
+                setLv_Adapter();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setLv_Adapter(){
+        KeranjangAdapter keranjangAdapter = new KeranjangAdapter(getActivity(),id_item,nama_barang,nama_toko,quantity,harga_barang);
+        lv_item_keranjang.setAdapter(keranjangAdapter);
+    }
 }
 
+class Barang{
+    private String name, id_toko;
+
+    public Barang(){
+
+    }
+
+    public Barang(String name, String id_toko){
+        this.name = name;
+        this.id_toko = id_toko;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getId_toko(){
+        return id_toko;
+    }
+
+    public void setId_toko(String id){
+        this.id_toko = id;
+    }
+}
+
+class IdBarang{
+    private String id_barang,id_item;
+    private int quantity, harga;
+
+    public IdBarang(){
+
+    }
+
+    public IdBarang(String id_barang, int qty, int harga){
+        this.id_barang = id_barang;
+        this.quantity = qty;
+        this.harga = harga;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void  setQuantity(int qty){
+        this.quantity = qty;
+    }
+
+    public int getHarga(){
+        return harga;
+    }
+
+    public void setHarga(int harga){
+        this.harga = harga;
+    }
+
+    public String getId(){
+        return id_item;
+    }
+
+    public void setId(String id_item){
+        this.id_item = id_item;
+    }
+
+    public String getId_barang(){
+        return id_barang;
+    }
+
+    public void setId_barang(String id_barang){
+        this.id_barang = id_barang;
+    }
+}
 
 class KeranjangAdapter extends BaseAdapter{
 
     Context context;
-    ArrayList<String> nama_barang,nama_toko;
-    ArrayList<Integer> quantity,harga_barang;
+    List<String> nama_barang,nama_toko;
+    List<Integer> quantity,harga_barang;
+    List<String> id_item;
     LayoutInflater inflater;
 
-    public KeranjangAdapter(Context context, ArrayList<String> nama_barang, ArrayList<String> nama_toko, ArrayList<Integer> quantity, ArrayList<Integer> harga_barang){
+    public KeranjangAdapter(Context context,List<String> id_item, List<String> nama_barang, List<String> nama_toko, List<Integer> quantity, List<Integer> harga_barang){
         this.context = context;
+        this.id_item = id_item;
         this.nama_barang = nama_barang;
         this.nama_toko = nama_toko;
         this.quantity = quantity;
@@ -124,7 +310,13 @@ class KeranjangAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        final int urut = i;
         View rootView = inflater.inflate(R.layout.keranjang_list_row,viewGroup,false);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference dr_delete = database.getReference();
 
         TextView tv_nama_barang = rootView.findViewById(R.id.tv_nama_barang);
         TextView tv_nama_toko = rootView.findViewById(R.id.tv_nama_toko);
@@ -135,6 +327,14 @@ class KeranjangAdapter extends BaseAdapter{
         tv_nama_toko.setText(nama_toko.get(i));
         tv_quantity.setText(String.valueOf(quantity.get(i))+" Kg");
         tv_harga_barang.setText("Rp "+String.valueOf(harga_barang.get(i)));
+
+        ImageView iv_delete = rootView.findViewById(R.id.iv_delete);
+        iv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dr_delete.child("User").child(user.getUid()).child("Keranjang").child("Item").child(id_item.get(urut)).removeValue();
+            }
+        });
         return rootView;
     }
 }
