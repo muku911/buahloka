c37416502cadb02bc057c9d626178db949170de9
Muhammad Fauzan <fauzan_htf@ymail.com>
2017-09-08 03:18:09 +0700

    Tambah Barang sama Data kebun intent salah

---
+++ Buahloka/app/src/main/java/com/calcatz/buahloka/TambahProduk.java
@@ -0,0 +1,13 @@
+package com.calcatz.buahloka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahProduk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_produk);
+    }
+}
